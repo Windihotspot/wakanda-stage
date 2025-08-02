bb3e578e842984124d3d6d09710d3fa2c069f66a
windihotspot <boiwindihotspot@gmail.com>
2025-08-02 20:52:08 +0100

    github workflow read and write

--- src/views/AcceptInviteSuccess.vue
+++ src/views/AcceptInviteSuccess.vue
@@ -68,14 +68,14 @@ const acceptInvite = async () => {
     const response = await axios.post(
       API_URL,
       payload,
-    console.log("response:", response)
+   
       {
         headers: {
           Authorization: `Bearer ${token}`
         }
       }
     )
-   
+    console.log("response:", response)
     ElNotification({
       title: 'Success',
       message: response.data.message || 'Invite accepted successfully!',
